8904270cd6a04082181f799bb0591cb9e4f6ea80
chenbeile <lebeichen@qq.com>
2020-11-15 20:29:58 +0800

    feat: 容器组件初始化

---
+++ src/components/TopView/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top-view">
+    <el-row gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮式展示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮式展示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮式展示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮式展示
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <top-view></top-view>
+    <sales-view></sales-view>
+    <bottom-view></bottom-view>
+    <map-view></map-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import TopView from '../components/TopView'
+import MapView from '../components/MapView'
+import BottomView from '../components/BottomView'
+import SalesView from '../components/SalesView'
 export default {
   name: 'Home',
   components: {
+    TopView,
+    MapView,
+    BottomView,
+    SalesView
     // HelloWorld
   }
 }
 </script>
+<style>
+ .home {
+   width:100%;
+   height: 100%;
+   padding: 20px;
+   background: #eee;
+   box-sizing: border-box;
+ }
+</style>
